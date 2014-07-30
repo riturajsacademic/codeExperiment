03887d03120981e96e6db1eb72a57a8992de4492
riturajsacademic <rituraj.s.academic@gmail.com>
2014-07-29 22:52:38 -0400

    new data.txt added

--- codeExample/src/codeExample/main.java
+++ codeExample/src/codeExample/main.java
@@ -1,10 +1,33 @@
 package codeExample;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Workdsds");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter type of sorting");
+			String s= in.nextLine();
+			System.out.println("The String is: "+s);
+			try {
+				System.out.println(br.readLine());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
